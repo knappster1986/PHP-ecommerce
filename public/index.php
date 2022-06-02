220703030c3755f055385d2b981e7333ef224e23
Adam Knapp <adam.knapp@hotmail.co.uk>
2022-06-02 16:20:02 +0100

    Move index into public folder

---
+++ public/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Shop Homepage</title>
+    <link rel="stylesheet" href="sass/app.css">
+</head>
+<body>
+    <header class="header">
+        <nav class="nav">
+            <a href="/" class="nav__logo">My Shop</a>
+            <ul class="nav__list">
+                <li class="nav__item">
+                    <a href="#" class="nav__link">Home</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__link">About</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__link">Shop</a>
+                </li>
+                <li class="nav__item">
+                    <a href="#" class="nav__link">Contact</a>
+                </li>
+            </ul>
+        </nav>
+    </header>    
+
+    <?php require_once("../resources/config.php"); ?>
+
+</body>
+</html>
---
+++ resources/functions.php
@@ -0,0 +1,9 @@
+<?php 
+
+
+if($connection) {
+    echo "is connected";
+}
+
+
+?>
